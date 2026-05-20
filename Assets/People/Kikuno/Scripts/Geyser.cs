a6796430fabea20acdc7a38726cc28286e9308a2
kikunosatoshi <kikuno.satos@gmail.com>
2026-05-20 16:23:48 +0900

    [fix]間欠泉のスクリプトを修正した

--- Assets/People/Kikuno/Scripts/Geyser.cs
+++ Assets/People/Kikuno/Scripts/Geyser.cs
@@ -2,19 +2,20 @@ using UnityEngine;
 
 public class Geyser : GimmickBase
 {
-    [SerializeField] private float _waterStartTime = 1f;
+    [SerializeField] private float _waterStartTime;
     [SerializeField] private float _waterKeepTime;
-    [SerializeField] private float _force = 10f;
+    [SerializeField] private float _force;
     [SerializeField] private int _maxSizeCount;
+
     private Rigidbody2D rb; //SerialzeField は　Unity内に項目を追加し、変更しやすくする
-    private float _time = 0;
+    private float _time;
     private float _keepTime;
     private Vector2 _startOffset;
     private BoxCollider2D _boxCol;
     private Vector2 _startBoxColSize;
-    ParticleSystem _ps;
-    ParticleSystemRenderer _renderer;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private ParticleSystem _ps;
+    private ParticleSystemRenderer _renderer;
+
     void Start()
     {
         _boxCol = GetComponent<BoxCollider2D>();
@@ -25,7 +26,6 @@ public class Geyser : GimmickBase
         _startOffset = _boxCol.offset;
     }
 
-    // Update is called once per frame
     void Update()
     {
         _time += Time.deltaTime;
@@ -33,24 +33,30 @@ public class Geyser : GimmickBase
         if (_time > _waterStartTime)
         {
             _keepTime += Time.deltaTime;
+
             if (_keepTime < _waterKeepTime)
             {
                 _ps.Play();
+
                 Bounds bounds = _renderer.bounds;
                 Vector3 size = bounds.size;
+
                 _boxCol.size = new Vector2(_startBoxColSize.x, size.y);
                 _boxCol.offset = new Vector2(_startOffset.x, _startOffset.y + size.y * 0.5f);
             }
             else
             {
                 _ps.Stop();
+
                 _boxCol.size = _startBoxColSize;
                 _boxCol.offset = _startOffset;
+
                 _time = 0f;
                 _keepTime = 0f;
             }
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         rb = collision.GetComponent<Rigidbody2D>();
