33dfb82163d382c5b2bbc1f4f7dc23061d8ab2b5
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-05-21 06:28:58 +0900

    [add]ステージマネージャーのセットを追加

--- Assets/People/Miyamoto/Scripts/Mission/MissionManager.cs
+++ Assets/People/Miyamoto/Scripts/Mission/MissionManager.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
 using Template.Editor;
 using UnityEngine;
 
 public class MissionManager : MonoBehaviour
 {
-    [SerializeReference, SubclassSelector] private List<IMission> _missions;
+    [SerializeReference, SubclassSelector] private IMission[] _missions;
+
+    private StageData _stageData;
 
     private void Awake()
     {
@@ -12,9 +15,14 @@ public class MissionManager : MonoBehaviour
         {
             mission.Initialize();
         }
+        _stageData = StageDataManager.GetCurrentStageData();
     }
     private void OnDestroy()
     {
-
+        for (var i = 0; i < _missions.Length; i++)
+        {
+            if (_missions[i].IsCompleted())
+                _stageData.SetClearInfo(i);
+        }
     }
 }
