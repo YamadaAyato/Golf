71640e1b7cc4b0f1571ee3e53f7796805da7358d
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-05-21 06:30:39 +0900

    [add]ステージを選ぶクラス

---
+++ Assets/People/Miyamoto/Scripts/StageSelect/StageSelector.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class StageSelector : MonoBehaviour
+{
+    [SerializeField] StageInfo[] _stageInfos;
+    private GolfInputAction _inputAction;
+    private int _currentStageIndex;
+    private void Awake()
+    {
+        _inputAction = new();
+        _stageInfos[0].Image.color = Color.red;
+    }
+    private void OnEnable()
+    {
+        _inputAction.Stage.Enable();
+        _inputAction.Stage.Move.started += OnMove;
+        _inputAction.Stage.Select.started += OnSelect;
+    }
+    private void OnDisable()
+    {
+        _inputAction.Stage.Disable();
+        _inputAction.Stage.Move.started -= OnMove;
+        _inputAction.Stage.Select.started -= OnSelect;
+    }
+    private void OnDestroy()
+    {
+        _stageInfos = null;
+        _inputAction.Dispose();
+    }
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        var dir = (int)ctx.ReadValue<float>();
+        var nextIndex = _currentStageIndex + dir;
+
+        if (nextIndex < 0 || nextIndex > _stageInfos.Length - 1)
+            return;
+
+        ImageUpdate(nextIndex);
+        SetCurrentIndex(nextIndex);
+    }
+    private void OnSelect(InputAction.CallbackContext ctx)
+    {
+        StageDataManager.SetStageInfo(_stageInfos[_currentStageIndex].StageData,
+                                      _stageInfos[_currentStageIndex].Stage);
+        SceneLoarder.LoadScene("Stage");
+    }
+    private void SetCurrentIndex(int nextIndex)
+    {
+        _currentStageIndex = nextIndex;
+    }
+    private void ImageUpdate(int nextIndex)
+    {
+        _stageInfos[_currentStageIndex].Image.color = Color.white;
+        _stageInfos[nextIndex].Image.color = Color.red;
+    }
+}
